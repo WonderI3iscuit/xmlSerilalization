6b3544560e9a40d2de5e7bde34f32e462137b345
WonderI3iscuit <58702197+WonderI3iscuit@users.noreply.github.com>
2020-10-29 13:27:39 -0400

    Add files via upload

---
+++ XML serialization/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace XML_serialization
+{
+
+    //serialize that hoe
+
+    [XmlRoot("New Employees", Namespace ="www.nerdinc.ninja")]
+    public class Employee //you cannot serialize private classes btw
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public double ssnumber { get; set; }
+        public DateTime EntryDate { get; set; } // using datetime method. predefined method in the .net framework
+        public string JobRole { get; set; }
+    }
+}
---
+++ XML serialization/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace XML_serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Employee emp = new Employee();
+     //     emp.ID = 123;
+     //     emp.name = "Ben";
+     //     emp.ssnumber = 123456789;
+     //     emp.EntryDate = DateTime.Now;
+     //     emp.JobRole = "ninja";
+
+
+            Console.WriteLine($"employee id: {emp.ID} \n employee name: {emp.name} \nEmployee ss#: {emp.ssnumber} \n date created {emp.EntryDate}");
+            Console.ReadKey();
+
+            //create a location to store the xml file
+            string FilePath = "C:/Temp/";
+            string FileName = "Samurai.xml";
+
+            //  //saves and creates the file path and combines the above tow variables
+            //  TextWriter writer = new StreamWriter(FilePath + FileName);
+            //
+            //  //actual part of the serialization
+            //  XmlSerializer ser = new XmlSerializer(typeof(Employee));
+            //
+            //  //get the writer stream and the instance of the class we have 
+            //  ser.Serialize(writer, emp);
+            //  writer.Close();
+            //
+            //  Console.ReadLine();
+            //
+
+            XmlSerializer des = new XmlSerializer(typeof(Employee));
+            using (XmlReader reader = XmlReader.Create(FilePath + FileName))
+            {
+                emp = (Employee)des.Deserialize(reader);
+                Console.WriteLine(($"employee id: {emp.ID} \n employee name: {emp.name} \nEmployee ss#: {emp.ssnumber} \n date created {emp.EntryDate}"));
+            }
+        }
+    }
+}
